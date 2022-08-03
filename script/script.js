9926df14409879c8fbea2b8acd0d307f04be303a
Sarangwala <sarangwalaa@student.douglascollege.ca>
2022-08-03 12:57:18 -0700

    check digit functions

--- script/script.js
+++ script/script.js
@@ -1,11 +1,21 @@
+function checkDigits(input){
+    return /^[0-9]+$/.test(input);
+}
+
+
 function addValue(){
     let input_a = document.getElementById('input_a').value;
     let input_b = document.getElementById('input_b').value;
+    let output;
+    let text;
+    if(!checkDigits(input_a) || !checkDigits(input_b)){
+        document.mycalculator.output.value = "input only digits please";
+        return;
+    }
     input_a = parseInt(input_a);
     input_b= parseInt(input_b);
-    let output = input_a+input_b;
-
-    let text = `${input_a} + ${input_b} = ${output}`;
+    output = input_a+input_b;
+    text = `${input_a} + ${input_b} = ${output}`;
 
     document.mycalculator.output.value = text;
 }
@@ -14,7 +24,10 @@ function addValue(){
 function mulValue(){
     let input_a = document.getElementById('input_a').value;
     let input_b = document.getElementById('input_b').value;
-
+    if(!checkDigits(input_a) || !checkDigits(input_b)){
+        document.mycalculator.output.value = "input only digits please";
+        return;
+    }
     let output = input_a*input_b;
     let text = `${input_a} * ${input_b} = ${output}`;
     document.mycalculator.output.value = text;
@@ -23,7 +36,10 @@ function mulValue(){
 function divValue(){
     let input_a = document.getElementById('input_a').value;
     let input_b = document.getElementById('input_b').value;
-
+    if(!checkDigits(input_a) || !checkDigits(input_b)){
+        document.mycalculator.output.value = "input only digits please";
+        return;
+    }
     let output = input_a/input_b;
     let text = `${input_a} / ${input_b} = ${output}`;
 
